cca032ff7dc513c4516ab4b3aa215af25ede1b30
Sethu1431 <18tucs220@skct.edu.in>
2020-07-20 21:33:56 -0700

    Day 1

---
+++ python.py
@@ -0,0 +1,36 @@
+# print
+print("Its my first Day")
+print("It\"s my first Day") #some error
+print("First" + "Day")
+print("Five"+ str(5))
+print(int('10')) #type conversion
+a=5
+print(a)
+a,b = 6,6
+print(a)
+print(b)
+#while loop
+x=2
+while x<10:
+    print(x)
+    x+=1
+#do while
+print("do while")
+x=1
+while True:
+    print(x)
+    x+=1
+    if(x > 5):
+        break
+print("do while end")    
+z=[11,12,13,14,15,15]
+#for loop
+for i in z:
+    print(i)
+#conditions    
+if a > b:
+    print("true")
+elif a == b:
+    print("a equal to b")    
+else:
+    print("false")
